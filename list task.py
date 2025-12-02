85cceff6a7796a6850cf08f53c3aca68709b75cd
Pavithran <pavithran.apdy@gmail.com>
2025-12-02 12:02:06 +0530

    list task

--- list task.py
+++ list task.py
@@ -1,18 +1,188 @@
-# #16. write a program to print list elements until the no. 50 appears (stop when found)
-# mylist=[2,4,5,33,44,55,22]
-# print("My List ", mylist)
-# count=""
+mylist = [3, 4, 5, 6, 44, 55, 22]
+print("My list= ", mylist)
+# # #1. write a programm to print all elements of a list using a loop
+# # print("1. write a programm to print all elements of a list using a loop")
+# # for i in mylist:
+# #     print("my list value is", i)
+
+# # #2. write a python program to print only the even nos. from a list
+# # print("2. write a python program to print only the even nos. from a list")
+# # count =""
+# # for i in mylist:
+# #     if i % 2 == 0:
+# #         s=str(i)
+# #         count+=s
+# #         #v=list(count)
+# # print("even nos. in a list", count)
+
+#3. write a python program to print only the odd nos. from a list
+print("3. write a python program to print only the odd nos. from a list")
+mylist = [3, 4, 5, 6, 44, 55, 22]
+count =[]
+for i in mylist:
+    if i % 2 != 0:
+        # s=str(i)
+        count.append(i)
+        #v=list(count)
+print("odd nos. in a list",count)
+  
+# # #4. write a programm to find the sum of all nos. in a list
+# # print("4. write a programm to find the sum of all nos. in a list")
+# # count = 0
+# # for i in mylist:
+# #     count+=i
+# # print("sum of all nos. in a list", count)
+
+# # #5. write a python program to find the largest no. in a list using a loop
+# # print("5. write a python program to find the largest no. in a list using a loop")
+# # count =mylist[0]
+# # for i in mylist:
+# #     if i > count:
+# #         count = i
+# # print("largest no. in a list",count)
+
+# # #6. write a python program to find the smallest no. in a list using a loop
+# # print("6. write a python program to find the smallest no. in a list using a loop")
+# # count =mylist[0]
+# # for i in mylist:
+# #     if i < count:
+# #         count = i
+# # print("smallest no. in a list",count)
+
+# # #7. write a python program to how many positive nos. in a list
+# # s=[2,4,7,-1,22,33,-3]
+# # print("7. write a python program to how many positive nos. in a list")
+# # print("my list is ", s)
+# # j=0
+# # for i in s:
+# #     if i >= 0:
+# #         j+=1
+# # print("No. of +ve Nos. in a list", j)
+
+# # #8. write a python program to how many negative nos. in a list
+# # print("8. write a python program to how many negative nos. in a list")
+# # j=0
+# # for i in s:
+# #     if i < 0:
+# #         j+=1
+# # print("No. of -ve Nos. in a list", j)
+
+# # #9. write a python program to reverse a list without using the .reverse() method
+# # print("9. write a python program to reverse a list without using the .reverse() method")
+# # print("mylist: ", mylist)
+# # print(mylist[::-1])
+
+# #10. write a python program to count how many even & odd nos. in a list
+# print("10. write a python program to count how many even & odd nos. in a list")
+# even_nos =0
 # for i in mylist:
-#   if i < 50:
-#     s=str(i)
-#     count+=s
-#   else:
+#     if i % 2 == 0:
+#         even_nos+=1
+# print("even nos. in a list ",even_nos)
+# odd_nos= len(mylist) - even_nos 
+# print("odd nos. in a list ",odd_nos)
+
+# # #11. write a python program to find and print the index of a specific value in a list
+# # print("11. write a python program to find and print the index of a specific value in a list")
+# # print("mylist", mylist)
+# # specific_value = int(input("enter specifie value in my list"))
+# # index=0
+# # for i in mylist:
+# #     index+=1
+# #     if i == specific_value:
+# #         print("index value of ", i,"is", index)
+
+# # #12. write a python program to replace all -ve nos. in a list with 0
+# # print("12. write a python program to replace all -ve nos. in a list with 0")
+# # print(s)
+# # count=""
+# # for i in s:
+# #     if i>0:
+# #         positive_value=str(i)
+# #         count+=positive_value
+# #     if i < 0:
+# #         i=0
+# #         negative_value=str(i)
+# #         count+=negative_value
+# # print("Replace of all -ve Nos. in a list with 0 ", count)
+
+# # #13. write a python program to print all Nos. greater than 50 in a list
+# # print("13. write a python program to print all Nos. greater than 50 in a list")
+# # print(mylist)
+# # count=""
+# # for i in mylist:
+# #     if i >= 50:
+# #         greaterthan_50 = str(i)
+# #         count += greaterthan_50
+# # print("print all Nos. greater than 50 in a list ", count)
+
+# #14. write a program to create a new list containing the squares of each element
+# s= [2, 3, 4, 5]
+# print("My List ", s)
+# count=""
+# for i in s:
+#   j=i**2
+#   print("new list containing the squares of each element",j)
+
+# # #15. write a python program to print all duplicate value in a list
+# # print("15. write a python program to print all duplicate value in a list")
+# # duplicate_value = [3,4,5,3,11,12,11]
+# # result = ""
+# # for ch in duplicate_value:
+# #    if ch not in duplicate_value:
+# #        result += str(ch)
+# #        print(ch)
+
+# # #16. write a program to print list elements until the no. 50 appears (stop when found)
+# # mylist=[2,4,5,33,44,55,22]
+# # print("My List ", mylist)
+# # count=""
+# # for i in mylist:
+# #   if i < 50:
+# #     s=str(i)
+# #     count+=s
+# #   else:
+# #     break
+# # print("print list elements until the no. 50 appears",count)
+
+# #17. write a python program to count how many nos. in a list are divisible by 5
+# print("17. write a python program to count how many nos. in a list are divisible by 5")
+# j=0
+# for i in mylist:
+#     if i %5 ==0:
+#         j+=1
+# print("No. of Nos. in a list are divisible by 5 is ", j)
+
+# #18. write a python program to find the second largest no. in a list using a loop
+# print("18. write a python program to find the second largest no. in a list using a loop")
+# max1 = max2 = 0
+# for i in mylist:
+#     if i > max1:
+#         max2 = max1
+#         max1 = i
+#     elif i > max2 and i != max1:
+#         max2 = i
+# print("2nd largest no. in a list", max2)
+
+# #19. To check whether the lis is assending order
+# print("19. To check whether the lis is assending order")
+# mylist1=[3,5,8,11,33,44]
+# for i in range(len(mylist1)-1):
+#   if mylist1[i] < mylist1[i+1]:
+#     print("list is assending order")
 #     break
-# print("print list elements until the no. 50 appears",count)
-#14. write a program to create a new list containing the squares of each element
-s= [2, 3, 4, 5]
-print("My List ", s)
-count=""
-for i in s:
-  j=i**2
-  print("new list containing the squares of each element",j)  
+#   else:
+#     print("list is not assending order") 
+
+# #20. write a python program to find the sum of  only the even nos. in a list
+# print("20. write a python program to find the sum of  only the even nos. ")
+# i_value =""
+# count=0
+# for i in mylist:
+#     if i % 2 == 0:
+#         count+=i
+#         s=str(i)
+#         i_value+=s
+#         #v=list(count)
+# print("even nos. in a list is", i_value ,"sum is ", count)
+
--- string_Task.py
+++ string_Task.py
@@ -10,7 +10,6 @@
 # #2. Write a program to reverse a string using a loop (without using slicing).
 # s=input("enter the any character ")           # input is pavi
 # print("using str.slicing ",s[:: -1])
-
 # count=""                #"" = empty String or count=0 zero value 
 # for i in s:
 #   count=i+count         # i is 0 count=p+ "";count=p, i=1 count=a+p; count=ap
@@ -35,21 +34,32 @@
 #    elif i.islower():
 #       lowercase+=1
 #    else:
-#       space+=1
-      
+#       space+=1  
 # print("uppercase in str ",uppercase)
 # print("lowercase in str ", lowercase)
 # print("space in str ", space)
 
 # 5. Write a program to remove all spaces from a string without using built-in replace().
-# s=input("enter any characters ")
-# case=""
-# for i in s:
-#    if i is " ":
-#       case+=""
-#    else:
-#       case+=i
-# print("remove all spaces in str "case)
+s=input("enter any characters ")
+case=""
+for i in s:
+   if i is " ":
+      case+=""
+   else:
+      case+=i
+print("remove all spaces in str "case)
+
+##print("6. To find the frequency of each character in a string")
+##string_value = input("Enter the string: ")
+##check = ""
+##for i in string_value:
+##    if i not in check:
+##        count = 0
+##        for j in string_value:
+##            if j == i:
+##                 count += 1
+##                 check += i
+##    print(i,":", count)
 
 # # #7. Write a program to count how many digits are present in a string.
 # s=input("enter the any character ")
@@ -58,13 +68,47 @@
 #   count=count+1
 # print("digits are present in a string ",count)
 
-# 10 Write a program to print all characters located at even indices of a string.
-s=input("enter any characters ")
-count=""
-for i in s:
-  print(i)
-  count=i+count
-print("without string slicing ",count)
+###8.Write a program to check if a string contains only alphabetic characters (without using isalpha())
+##
+##print("8. To check if a string contains only alphabetic characters (without using isalpha())")
+##string_value = input("Enter the string : ")
+##only_alphabet = True
+##for i in string_value:
+##        if not ('a' <= i <= 'z' or 'A' <= i <= 'Z'):
+##                only_alphabet = False
+##                break
+##if only_alphabet:
+##        print("Has only alphabet")
+##else:
+##        print("Not only alphabet")
+
+###9. Write a program to convert all lowercase letters in a string to uppercase manually using ASCII values.
+##
+##print("9. To convert all lowercase letters in a string to uppercase manually using ASCII values.")
+##string_value = input("Enter the string : ")
+##result = ""
+##for i in string_value:
+##    if i >= "a" and i <= "z":
+##        result += chr(ord(i) - 32)
+##    else:
+##        result += i
+##print(result)
+
+# # 10 Write a program to print all characters located at even indices of a string.
+# s=input("enter any characters ")
+# count=""
+# for i in s:
+#   print(i)
+#   count=i+count
+# print("without string slicing ",count)
+###11.Write a program to count the number of words in a string without using split().
+##print("11. To count the number of words in a string without using split()")          
+##string_value = input("Enter the sentence: ")
+##count = 1
+##for ch in string_value:
+##    if ch == " ":
+##        count += 1
+##print("No. of words:", count)
 
 # #12. Write a program to replace all vowels in a string with * using a loop.
 # s=input("enter the any character ")
@@ -76,3 +120,76 @@ print("without string slicing ",count)
 #   else: 
 #       count=count+"*"
 # print("vowel in string ",count)
+###13.Write a program to find the longest word in a sentence
+##print("13. To find the longest word in a sentence")
+##s = input("Enter the sentence: ")
+##words = s.split()
+##res = ""
+##for word in words:
+##    if len(word) > len(res):
+##            res = word
+##print("Longest word:", res)
+
+###14.Write a program to check whether two strings are anagrams of each other using loops.
+##print("14. To check whether two strings are anagrams of each other using loops.")
+##s1 = input("Enter first string: ")
+##s2 = input("Enter second string: ")
+##
+##if len(s1) != len(s2):
+##    print("Not Anagrams")
+##else:
+##    for ch in s1:
+##        if ch not in s2:
+##            print("Not Anagrams")
+##            break
+##    else:
+##        print("Anagrams")
+
+###15.Write a program to count how many special characters are present in a string.
+##print("15. To count how many special characters are present in a string")
+##sting_value = input("Enter the string: ")
+##count = 0
+##for ch in sting_value:
+##    if not ch.isalnum():
+##        count += 1
+##print("No. Special characters in str: ", count)
+
+###16. Write a program to print each character of a string a given number of times
+##print("16. To print each character of a string a given number of times")
+##string_value = input("Enter the string: ")
+##n = int(input("How many times: "))
+##for i in string_value:
+##    print(i * n)
+
+###17. Write a program to remove duplicate characters from a string using loops
+##print("17. To remove duplicate characters from a string using loops")
+##string_value = input("Enter the string: ")
+##result = ""
+##for ch in string_value:
+##    if ch not in result:
+##        result += ch
+##print(result)
+
+###18. Write a program to count the number of consonants in a given string.
+##print("18. To count the number of consonants in a given string.")
+##string_value = input("Enter the string: ")
+##count = 0
+##for ch in string_value:
+##    if ch.isalpha() and ch not in "aeiou" and ch not in "AEIOU":
+##        count += 1
+##print("Consonants:", count)
+
+###19.Write a program to capitalize the first letter of a string manually (without using capitalize())
+##
+##print("19. To capitalize the first letter of a string manually (without using capitalize())")
+##string_value = input("Enter the string: ")
+##first = string_value[0].upper()
+##print(first + string_value[1:])
+
+# #20. Write a program to print characters of a string until a vowel is encountered.
+# print("20. To print characters of a string until a vowel is encountered.")
+# string_value = input("Enter the string: ")
+# for i in string_value:
+#     if i in "aeiouAEIOU":
+#         break
+#     print(i)
